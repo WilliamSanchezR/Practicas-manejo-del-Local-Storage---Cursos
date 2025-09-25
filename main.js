5ae0203e6e14d791501ef024a66fffad6df3f2f1
William Sanchez <183452295+WilliamSanchezR@users.noreply.github.com~>
2025-09-25 13:26:05 -0500

    Cree la funcion en Js para que en un array se guarden los datos de un formalario que a su vez esa informacion se guarde en el localStorage

--- main.js
+++ main.js
@@ -1,53 +1,44 @@
 // Declaramos las variables
 
-const  formulario = document.getElementById("formulario");
-const input = document.getElementById("nombre");
-const saludo = document.getElementById("saludo");
-const eliminar = document.getElementById("borrar");
+const formulario = document.getElementById("formulario");
+const inputCurso = document.getElementById("curso");
+const inputProfesor = document.getElementById("profesor");
+const inputPrecio = document.getElementById("precio");
+const inputCiudad = document.getElementById("ciudad");
+const inputCupo = document.getElementById("cupo");
+const h2 = document.getElementById("mensaje-curso");
+const borrar = document.getElementById("borrar-curso");
 
 
 // Le damos un evento de escucha al formulario para que cada ves que le den guardar se guarde lo guardado en el input y lo muetre en h2 donde esta el id saludo
 
-
-
 formulario.addEventListener("submit", (e) => {
-
     e.preventDefault();
+    const curso=inputCurso.value.trim();
+    const profesor=inputProfesor.value.trim();
+    const precio=inputPrecio.value.trim();
+    const ciudad=inputCiudad.value.trim();
+    const cupo=inputCupo.value.trim();
 
-    const nombre= input.value.trim();
-
-    if (nombre == "") {
-
-        alert("Por fa deja de ser tan pato, y llena todos los espacios del formulario")
-
+    if (curso === "" || profesor ==="" || precio === "" || ciudad == "" || cupo === "") {
+        alert("Por favor llena todo los espacios del formulario")
         return
     }
 
-    localStorage.setItem("usuario", nombre);
 
-    saludo.textContent = "Bienvenido " + nombre ;
+// en el ejericio anterior solo era un valor ahora estamos alimentando un objeto con sus diferentes claves y valores
 
-    input.value = "";
+    const nuevoCurso = {
+        nombre : curso,
+        profesor : profesor,
+        precio : precio,
+        ciudad : ciudad,
+        cupo : cupo
+    };
 
-});
-    
+    localStorage.setItem("curso", JSON.stringify(nuevoCurso));
 
-
-// Para esta nueva funcion al documento le damos un evento de escucha donde si la pagina se reinicia en el elemento capturado con el id saludo nos muestre el mensaje 
-
-document.addEventListener("DOMContentLoaded", () => {
-
-    const usuarioGuardado = localStorage.getItem("usuario");
-
-    if (usuarioGuardado) {
-        saludo.textContent = "Bienvenido de nuevo " + usuarioGuardado;
-    }
+    formulario.reset();
 
 });
 
-borrar.addEventListener("click", () => {
-
-    localStorage.removeItem("usuario");
-
-    saludo.textContent = "Bienvenido usuario indefinido"; 
-});
